39b2b40de3a4095cea6bfab26e60e76a1b6cc5ad
rpprppprppr <kondrenkodo@gmail.com>
2025-04-08 02:20:36 +0500

    finished mainBanner component (adaptive)

--- src/components/MainBanner.vue
+++ src/components/MainBanner.vue
@@ -1,7 +1,78 @@
-<script setup></script>
+<script setup>
+  import photo from "@/assets/img/main-banner-photo.png";
+</script>
 
 <template>
-  <h1>MainBanner</h1>
+  <div class="main-banner">
+    <div class="left">
+      <img class="main-banner__photo" :src="photo" alt="Баннер">
+    </div>
+    <div class="right">
+      <div class="main-banner__text"> 
+        <span class="text-brand">THE BRAND</span><br> OF LUXERIOUS <span style="color: #F16D7F;">FASHION</span> 
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+  .main-banner {
+    background-color: #F1E4E6;
+    display: flex;
+    gap: 60px;
+  }
+
+  .main-banner__text {
+    text-align: left;
+    font-weight: bold;
+    color: #222222;
+    font-size: 32px;
+    margin-top: 290px;
+    padding-left: 16px;
+    border-left: 12px solid #F16D7F;
+  }
+
+  .text-brand {
+    font-size: 48px;
+    font-family: "Lato Black", sans-serif;
+  }
+
+  @media screen and (max-width: 1599px) {
+    .main-banner {
+      justify-content: center;
+      gap: 40px;
+    }
+
+    .main-banner__photo {
+      width: 386px;
+    }
+
+    .main-banner__text {
+      font-size: 24px;
+      margin-top: 130px;
+    }
+
+    .text-brand {
+      font-size: 44px;
+    }
+  }
+
+  @media screen and (max-width: 767px) {
+    .main-banner {
+      height: 360px;
+    }
+
+    .left {
+      display: none;
+    }
+
+    .main-banner__text {
+      font-size: 20px;
+      margin-top: 150px;
+    }
+
+    .text-brand {
+      font-size: 38px;
+    }
+  }  
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,13 @@
 <script setup>
+  import MainBanner from "@/components/MainBanner.vue";
   import DefaultLayout from "@/layouts/Default.vue";
 </script>
 
 <template>
   <DefaultLayout>
+    <MainBanner />
     <h1 class="h1">Home</h1>
   </DefaultLayout>
 </template>
 
-<style scoped lang="scss">
-  .h1 {
-    color: red;
-  }
-</style>
+<style scoped lang="scss"></style>
