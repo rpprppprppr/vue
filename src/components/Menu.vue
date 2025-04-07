de212e8493c7378684494d2d0ec1e9e8e79ba210
rpprppprppr <kondrenkodo@gmail.com>
2025-04-07 18:17:37 +0500

    finished menu component (adaptive)

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -21,25 +21,27 @@
 <template>
   <div v-if="isOpen" class="menu__overlay" @click.self="closeMenu">
     <div class="menu__container">
-      <button class="close-button" @click="closeMenu"><img :src="closeIcon" alt="Закрыть" /></button>
-      <div class="menu-title">MENU</div>
-      <div class="menu__nav">
-        <div class="category__section">
-          <span class="category">MAN</span>
-          <div class="items-list">
-            <div v-for="item in manItems" :key="item">{{ item }}</div>
+      <div class="menu">
+        <button class="close-button" @click="closeMenu"><img :src="closeIcon" alt="Закрыть" /></button>
+        <div class="menu-title">MENU</div>
+        <div class="menu__nav">
+          <div class="category__section">
+            <span class="category">MAN</span>
+            <div class="items-list">
+              <div v-for="item in manItems" :key="item">{{ item }}</div>
+            </div>
           </div>
-        </div>
-        <div class="category__section">
-          <span class="category">WOMAN</span>
-          <div class="items-list">
-            <div v-for="item in womanItems" :key="item">{{ item }}</div>
+          <div class="category__section">
+            <span class="category">WOMAN</span>
+            <div class="items-list">
+              <div v-for="item in womanItems" :key="item">{{ item }}</div>
+            </div>
           </div>
-        </div>
-        <div class="category__section">
-          <span class="category">KIDS</span>
-          <div class="items-list">
-            <div v-for="item in kidsItems" :key="item">{{ item }}</div>
+          <div class="category__section">
+            <span class="category">KIDS</span>
+            <div class="items-list">
+              <div v-for="item in kidsItems" :key="item">{{ item }}</div>
+            </div>
           </div>
         </div>
       </div>
@@ -54,18 +56,28 @@
     right: 0;
     width: 100%;
     height: 100%;
-    display: flex;
-    justify-content: flex-end;
+    
     z-index: 1000;
   }
 
   .menu__container {
+    min-height: 100vh;
+    min-width: 375px;
+    max-width: 1600px;
+    width: 100%;
+    margin: 80px auto 0;
+    text-align: center;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  .menu {
     background: white;
     width: 232px; 
-    height: 764px; 
+    height: 730px; 
     padding: 16px;
     padding-left: 34px;
-    margin-top: 80px; 
     box-shadow: -2px 0 10px rgba(0, 0, 0, 0.1);
     position: relative;
     display: flex;
@@ -107,5 +119,26 @@
 
   .category {
     color: #F16D7F;
+  }
+
+  @media screen and (max-width: 767px) {
+    .menu__container {
+      margin-top: 80px;
+      display: block;
+    }
+
+    .menu {
+      width: 100%;
+      height: fit-content;
+      padding-left: 0;
+      padding: 25px;
+      text-align: center;
+      box-sizing: border-box;
+      font-size: 15px;
+    }
+
+    .items-list {
+      margin-left: 0;
+    }
   }
 </style>
