c51ef467e040e4d10b8d73b6567c34d60719d838
rpprppprppr <kondrenkodo@gmail.com>
2025-06-01 21:07:31 +0500

    fix hot reload

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,10 @@ export default defineConfig({
   },
   server : {
     host: true,
-    port: "5173",
+    port: 5173,
+    watch: {
+      usePolling: true,
+    },
     proxy: {
       '/api/': {
         target: 'https://fakestoreapi.com',
